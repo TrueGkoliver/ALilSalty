7f2ead1580b2c7970b7d93090a927ccdd279276c
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-09-25 15:54:43 -0500

    registries

---
+++ src/main/java/com/gkoliver/alilsalty/core/registry/LilSaltyBlocks.java
@@ -0,0 +1,11 @@
+package com.gkoliver.alilsalty.core.registry;
+
+import com.gkoliver.alilsalty.ALilSalty;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class LilSaltyBlocks {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ALilSalty.MOD_ID);
+}
---
+++ src/main/java/com/gkoliver/alilsalty/core/registry/LilSaltyItems.java
@@ -0,0 +1,10 @@
+package com.gkoliver.alilsalty.core.registry;
+
+import com.gkoliver.alilsalty.ALilSalty;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class LilSaltyItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ALilSalty.MOD_ID);
+}
