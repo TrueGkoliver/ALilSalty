8a83c128096985273ac7a8ed188eeb98da2998c6
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-09-26 10:54:11 -0500

    No more falling if touching water

--- src/main/java/com/gkoliver/alilsalty/common/block/SaltBlock.java
+++ src/main/java/com/gkoliver/alilsalty/common/block/SaltBlock.java
@@ -1,13 +1,18 @@
 package com.gkoliver.alilsalty.common.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.entity.CreatureAttribute;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.FallingBlockEntity;
 import net.minecraft.entity.monster.SlimeEntity;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 import java.util.Random;
 
@@ -32,6 +37,31 @@ public class SaltBlock extends FallingBlock {
                     });
                 }
 
+            }
+        }
+    }
+    public static boolean isTouching(Block toLookFor, World reader, BlockPos pos) {
+        BlockPos stateNorth = pos.north();
+        BlockPos stateSouth = pos.south();
+        BlockPos stateWest = pos.west();
+        BlockPos stateEast = pos.east();
+        BlockPos stateUp = pos.up();
+        BlockPos stateDown = pos.down();
+        if (reader.getBlockState(stateNorth).getBlock()==toLookFor) return true;
+        if (reader.getBlockState(stateSouth).getBlock()==toLookFor) return true;
+        if (reader.getBlockState(stateWest).getBlock()==toLookFor) return true;
+        if (reader.getBlockState(stateEast).getBlock()==toLookFor) return true;
+        if (reader.getBlockState(stateUp).getBlock()==toLookFor) return true;
+        if (reader.getBlockState(stateDown).getBlock()==toLookFor) return true;
+        return false;
+    }
+    @Override
+    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
+        if (worldIn.isAirBlock(pos.down()) || canFallThrough(worldIn.getBlockState(pos.down())) && pos.getY() >= 0) {
+            if (!isTouching(Blocks.WATER, worldIn, pos)) {
+                FallingBlockEntity fallingblockentity = new FallingBlockEntity(worldIn, (double)pos.getX() + 0.5D, (double)pos.getY(), (double)pos.getZ() + 0.5D, worldIn.getBlockState(pos));
+                this.onStartFalling(fallingblockentity);
+                worldIn.addEntity(fallingblockentity);
             }
         }
     }
---
+++ src/main/java/com/gkoliver/alilsalty/common/block/SaltPileBlock.java
@@ -0,0 +1,9 @@
+package com.gkoliver.alilsalty.common.block;
+
+import net.minecraft.block.Block;
+
+public class SaltPileBlock extends Block {
+    public SaltPileBlock(Properties properties) {
+        super(properties);
+    }
+}
