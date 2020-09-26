b53ced3a57c4cca7ca4ca1ddaee432583ef1d5ca
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-09-26 09:52:19 -0500

    mixin

--- src/main/java/com/gkoliver/alilsalty/common/ai/AvoidSaltBlockGoal.java
+++ src/main/java/com/gkoliver/alilsalty/common/ai/AvoidSaltBlockGoal.java
@@ -1,4 +1,9 @@
 package com.gkoliver.alilsalty.common.ai;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+
 public class AvoidSaltBlockGoal {
+    //TODO big placeholder
+
 }
---
+++ src/main/java/com/gkoliver/alilsalty/common/mixin/WalkNodeProcessorMixin.java
@@ -0,0 +1,31 @@
+package com.gkoliver.alilsalty.common.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
+import net.minecraft.pathfinding.PathNodeType;
+import net.minecraft.pathfinding.WalkNodeProcessor;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(WalkNodeProcessor.class)
+public class WalkNodeProcessorMixin {
+    public static final Block SALT = Blocks.MAGMA_BLOCK;
+    @Inject(method="", at=@At("HEAD"))
+    protected static void evaluate(IBlockReader blockReader, BlockPos pos, CallbackInfoReturnable<PathNodeType> ci) {
+        BlockState blockstate = blockReader.getBlockState(pos);
+        PathNodeType type = blockstate.getAiPathNodeType(blockReader, pos);
+        if (type != null) ci.setReturnValue(type);
+        Block block = blockstate.getBlock();
+        Material material = blockstate.getMaterial();
+        if (block==SALT) {
+            ci.setReturnValue(PathNodeType.UNPASSABLE_RAIL);
+        }
+    }
+}
