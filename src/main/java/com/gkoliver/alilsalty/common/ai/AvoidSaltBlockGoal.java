2773ead2eea069128d15696ec3c498c97dac933c
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-09-27 10:02:45 -0500

    Deleted AvoidSaltBlockGoal, it's not needed

--- src/main/java/com/gkoliver/alilsalty/common/ai/AvoidSaltBlockGoal.java
+++
@@ -1,9 +0,0 @@
-package com.gkoliver.alilsalty.common.ai;
-
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-
-public class AvoidSaltBlockGoal {
-    //TODO big placeholder
-
-}
