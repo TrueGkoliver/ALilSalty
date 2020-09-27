0a96a6f27d1ce4e94a13b32cfcbb70306faac32f
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-09-27 09:50:51 -0500

    Salt pile block basics

--- src/main/java/com/gkoliver/alilsalty/common/block/SaltPileBlock.java
+++ src/main/java/com/gkoliver/alilsalty/common/block/SaltPileBlock.java
@@ -1,9 +1,29 @@
 package com.gkoliver.alilsalty.common.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
+import net.minecraft.world.server.ServerWorld;
+
+import java.util.Random;
 
 public class SaltPileBlock extends Block {
+    private static final VoxelShape SHAPE = Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 1.0D, 13.0D);
     public SaltPileBlock(Properties properties) {
         super(properties);
+        properties.tickRandomly();
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerWorld worldIn, BlockPos pos, Random random) {
+        if (worldIn.canSeeSky(pos) && worldIn.isRainingAt(pos)) {} ;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
     }
 }
