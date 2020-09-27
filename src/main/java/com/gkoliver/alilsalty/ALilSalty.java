d258fe8561a6555298068164575e42001da95858
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-09-27 18:24:24 -0500

    Stabilizing code because I have nothing else to do lol

--- src/main/java/com/gkoliver/alilsalty/ALilSalty.java
+++ src/main/java/com/gkoliver/alilsalty/ALilSalty.java
@@ -15,6 +15,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -23,6 +24,7 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -44,6 +46,8 @@ public class ALilSalty
     }
 
     private void setup(final FMLCommonSetupEvent event)  {
-        LilSaltyFeatures.saltOreAddLoop();
+        DeferredWorkQueue.runLater(()->{
+            LilSaltyFeatures.saltOreAddLoop();
+        });
     }
 }
