48ab33fa789a1b89530672be3e37a1f17511577f
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-09-27 15:48:57 -0500

    Fixed a mixin problem

--- src/main/java/com/gkoliver/alilsalty/common/mixin/WalkNodeProcessorMixin.java
+++ src/main/java/com/gkoliver/alilsalty/common/mixin/WalkNodeProcessorMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(WalkNodeProcessor.class)
 public class WalkNodeProcessorMixin {
-    public static final Block SALT = Blocks.MAGMA_BLOCK;
+    private static final Block SALT = Blocks.MAGMA_BLOCK;
     @Inject(method="func_237238_b_", at=@At("HEAD"))
     protected static void evaluate(IBlockReader blockReader, BlockPos pos, CallbackInfoReturnable<PathNodeType> ci) {
         BlockState blockstate = blockReader.getBlockState(pos);
